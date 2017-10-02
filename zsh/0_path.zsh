9ce7c378980c109987dc6ec1193b89653d8ad820
Nicolas Widart <n.widart@gmail.com>
2017-10-02 21:16:58 +0200

    Adding global composer bin path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -23,3 +23,4 @@ PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS;
 pathAppend "$HOME/.yadr/bin"
 pathAppend "$HOME/.yadr/bin/yadr"
 pathAppend "/Applications/MAMP/bin/php/php7.0.15/bin"
+pathAppend "$HOME/.composer/vendor/bin"
